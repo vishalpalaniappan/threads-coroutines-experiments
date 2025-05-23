5efbf0db14dfe2f312664fb35be018cb59007af4
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-23 00:34:50 -0400

    Add a test function to threads example2.

--- threads_example/example2/Thread1.py
+++ threads_example/example2/Thread1.py
@@ -3,6 +3,10 @@ import queue
 import time
 import random
 
+def testFunc():
+    print("asdf")
+    a = 1
+
 class Thread1(threading.Thread):
     def __init__(self, id, queue):
         super().__init__(daemon=True)
@@ -17,4 +21,5 @@ class Thread1(threading.Thread):
         for i in range(5):
             time.sleep(random.uniform(0.5, 1.5))
             self.queue.put(f"Thread {self.id} - message {i}")
+            testFunc()
         self.queue.put(f"Thread {self.id} - DONE")
