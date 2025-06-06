4f16ebbdb93c7563f3e975d6cfb1c65df39e7ac3
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-06-06 02:53:18 -0400

    Update example1 and remove example2_await.

--- coroutines_example/example1/example1.py
+++ coroutines_example/example1/example1.py
@@ -1,21 +1,41 @@
 import asyncio
+import threading
+
+def test():
+    task = asyncio.current_task()
+    print("TESTING:", task.get_name())
 
 async def task1():
-    print("Starting task 1")
+    task = asyncio.current_task()
+    print(f"Starting task {asyncio.current_task().get_name()}")
+    test()
     await asyncio.sleep(3)
-    print("Finished task 1")
+    print(f"Finished task {asyncio.current_task().get_name()}")
+    test()
 
 async def task2():
-    print("Starting task 2")
+    print(f"Starting task {asyncio.current_task().get_name()}")
+    test()
     await asyncio.sleep(2)
-    print("Finished task 2")
+    print(f"Finished task {asyncio.current_task().get_name()}")
+    test()
 
 async def task3():
-    print("Starting task 3")
+    print(f"Starting task {asyncio.current_task().get_name()}")
+    test()
     await asyncio.sleep(7)
-    print("Finished task 3")
+    print(f"Finished task {asyncio.current_task().get_name()}")
+    test()
 
 async def main():
     await asyncio.gather(task1(), task2(), task3())
+    
+    b = 2
 
-asyncio.run(main())
+
+def asdf():
+    asyncio.run(main())
+
+asdf()
+
+print('asdf')
